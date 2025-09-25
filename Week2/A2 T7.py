ccf72778bfc116c772f6a05f04c5e8b4083d790b
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2025-09-25 08:58:28 +0300

    Add files via upload
    Week 2 Task 7

---
+++ Week2/A2 T7.py
@@ -0,0 +1,11 @@
+#Program starting.
+#Insert fahrenheits: 50
+#0.0°F is 10.0°C
+#Program ending.
+print ("Program starting.")
+Fahrenheit = float(input("Insert fahrenheits: \n"))
+Celsius =  (Fahrenheit - 32) / 1.8
+Celsius = round(Celsius, 1)
+print (f"{Fahrenheit}°F is {Celsius}°C")
+print("Program ending.")
+              
