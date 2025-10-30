255764b5f113c69a15c690e821b61d08d07b0511
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2025-10-30 09:42:46 +0200

    Update A6 Task 2.py

--- Week 6/A6 Task 2.py
+++ Week 6/A6 Task 2.py
@@ -1,1 +1,10 @@
+#A6 Task 2
+print("Program starting.")
+FirstName = input("Insert first name: ")
+SecondName = input("Insert last name: ")
+FileName = input("Insert file name: ")
+file = open(FileName, "w")
+file.write(FirstName + "\n")
+file.write(SecondName + "\n")   
+file.close()
 
