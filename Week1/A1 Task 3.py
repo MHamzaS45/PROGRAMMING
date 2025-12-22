2f43fcfadd9aaf028742be01f1c3150aa94075db
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2025-12-23 02:31:28 +0500

    Create A1 Task 3.py

---
+++ Week1/A1 Task 3.py
@@ -0,0 +1,8 @@
+#A1_T3 Ask name
+#Create a Python program which:
+#Asks “What is your name: ” and collects the input value into Name variable
+#Prints message: “Hi there {Name}”
+#Note! the test the code multiple times with different inputs.
+
+Name = input("What is your name: ")
+print(f"Hi there {Name}")
