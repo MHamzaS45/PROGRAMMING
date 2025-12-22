7524695a56f964febfbd447a32ed05304ee8d41b
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2025-12-23 02:37:30 +0500

    Create A1 Task 4.py

---
+++ Week1/A1 Task 4.py
@@ -0,0 +1,12 @@
+#A1 Task 4
+#Printing and math operations
+
+Num1 = 47
+Num2 = 102
+Sum = Num1 + Num2
+print(f" {Num1} + {Num2} = {Sum} ")
+Diff = Num2 - Num1
+print(f" {Num2} - {Num1} = {Diff} ")
+Product = Sum * Diff 
+print(f" {Num1} * {Num2} = {Product} ")
+print(f" ( {Num1} + {Num2} ) * ( {Num2} - {Num1} ) = {Product} ") # Print the sum, sub and multiply operations together. 
