00ba834eee49b99b8e6f046672b4789a58279b64
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2025-12-25 01:26:19 +0500

    Update A1 Task 7.py

--- Week1/A1 Task 7.py
+++ Week1/A1 Task 7.py
@@ -1,3 +1,15 @@
 #A1 Task 7
 # Fuel consumption
-
+print ("Calculate fuel consumption")
+Feed = input("Enter travel distance (kilometers): ")
+Distance = int(Feed)
+if Distance < 0:
+    print("Error: Distance must be a positive integer.")    
+else:
+    Feed = input("Enter fuel usage (liters): ")
+    Fuel = int(Feed)
+    if Fuel < 0:
+        print("Error: Fuel consumption must be a positive integer.")
+    else:
+        Consumption = (Fuel / Distance) * 100
+        print(f"Total fuel consumption: {Consumption} l per 100 km")
