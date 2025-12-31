966d3dc2332f6b40339d1657268e1c03526663b3
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2026-01-01 03:34:40 +0500

    Update A9 Task 6.py

--- Week 9/A9 Task 6.py
+++ Week 9/A9 Task 6.py
@@ -1,1 +1,48 @@
+#A9 Task 6.py
+#Save before exit - Keyboard Interrupt error handling
 
+def saveLines(Lines: list[str], Filename: str) -> None:
+    with open(Filename, "w", encoding="utf-8") as file:
+        for line in Lines:
+            file.write(line + "\n")
+
+def main() -> None:
+    Lines: list[str] = []
+    print("Program starting.")
+    try:
+        while True:
+            print("Options:")
+            print("1 - Insert line")
+            print("2 - Save lines")
+            print("0 - Exit")
+            choice = input("Your choice: ")
+            if choice == "1":
+                text = input("Insert text: ")
+                Lines.append(text)
+            elif choice == "2":
+                if len(Lines) == 0:
+                    print("No lines to save.")
+                else:
+                    filename = input("Insert filename: ")
+                    saveLines(Lines, filename)
+
+            elif choice == "0":
+                break
+
+            else:
+                print("Invalid option.")
+
+    except KeyboardInterrupt: # Catch keyboard interrupt
+        print("Keyboard interrupt and unsaved progress!")
+        if len(Lines) == 0:
+            print("Program ending.")
+            return
+        answer = input("Save before quit(y/n)?: ").lower()
+        if answer == "y":
+            filename = input("Insert filename: ")
+            saveLines(Lines, filename)
+
+
+if __name__ == "__main__":
+    main()
+    print("\nProgram ending.")
