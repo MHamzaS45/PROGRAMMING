46100d0556394a4a72358dbb6b06616cf940fc4f
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2025-12-31 19:40:58 +0500

    Update A10 Task 1.py

--- Week 10/A10 Task 1.py
+++ Week 10/A10 Task 1.py
@@ -1,1 +1,46 @@
+# A10 Task 1.py
+# Developer: Muhammad Hamza Sahqani
+# Date: 2025-12-21
+import sys
+
+def display():
+    global filename
+    horizontal_box = []
+    while True:
+        try:
+            filename = input("Insert filename: ").strip()
+            try:
+                with open(filename,"r") as file
+                    print("#---Vertically---#")
+                    for line in file:
+                        line = line.strip()
+                        if line: 
+                            print(line)
+                            horizontal_box.append(line)
+                    print("#---Vertically---#")
+                print("#---Horizontally---#")
+                int_num = [int(num) for num in horizontal_box] 
+                print_out = " , ".join(str(num) for num in int_num)
+                print(print_out)
+                print("#---Horizontally---#")
+                break
+            except FileNotFoundError:
+                print(f"File \"{filename}\" not found. Try again!")
+            except PermissionError:
+                print("You do not have permission to access this file.")
+        except ValueError:
+            print("Please insert a filename!")
+        except KeyboardInterrupt:
+            print("User interupt the program.")
+            print("Program ending.")
+            sys.exit(0)
+
+def main(): # Main function
+    print("Program starting.")
+    display()
+    print("\nProgram ending.")
+
+
+if __name__ == "__main__":
+    main()
 
