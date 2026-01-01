dda4ab51f74c11a2f370a762c3c8729d6a6d9abb
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2026-01-01 11:55:20 +0500

    Update A9 Task 3.py

--- Week 9/A9 Task 3.py
+++ Week 9/A9 Task 3.py
@@ -1,1 +1,32 @@
+########################################################
+# Task A9_T3
+# Developer Hamza Sahqani
+# Date 2025-12-25
+########################################################
+import sys
 
+def requestFilename() -> str: #checks  to see if text file exists
+    while True:
+        Feed = input("Insert filename: ").strip()
+        try:
+            with open(Feed, "r", encoding="utf-8") as file:
+                return Feed
+        except FileNotFoundError:
+            print(f"Couldn't read file '{Feed}'. ")
+
+def printFileContents(filename: str) -> None: 
+    print(f"## {filename} ##")
+    with open(filename, "r", encoding="utf-8") as file:
+        for row in file:
+            print(row.strip()) #Should print "File exists" in the case of A3 T3 D1.txt
+    print(f"## {filename} ##")
+
+def main() -> None:
+    print("Program starting.")
+    filename = requestFilename()
+    printFileContents(filename)
+    print("Program ending.")
+
+
+if __name__ == "__main__":
+    main()
