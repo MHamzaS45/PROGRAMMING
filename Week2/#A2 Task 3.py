8bd1caa881e503cb98ad9558f0c83cb57c79e04b
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2026-01-01 17:21:08 +0500

    Create #A2 Task 3.py

---
+++ Week2/#A2 Task 3.py
@@ -0,0 +1,10 @@
+#A2 Task 3.py
+
+print("Program starting.")
+FirstFeed = input("Insert first word: ")
+SecondFeed = input("Insert second word: ")
+print ("1st word is", len(FirstFeed), " characters long.") #calls letters
+print("2nd word is", len(SecondFeed), "characters long.") #calls letters
+Sum = FirstFeed + SecondFeed 
+print(f"Words together makes one closed compound '{Sum}'. ")
+print("Program ending.")
